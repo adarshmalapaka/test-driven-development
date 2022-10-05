504133cbf7f6c9a1c0fa2040d8e9a953820d1cdc
Rishabh Singh <92544924+rish2911@users.noreply.github.com>
2022-10-05 08:50:45 -0400

    Class implementation (#1)

--- include/PID.hpp
+++ include/PID.hpp
@@ -5,65 +5,66 @@
  * @version 0.2
  * @date 2022-10-02
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
-#ifndef PID_HPP_
-#define PID_HPP_
+#ifndef INCLUDE_PID_HPP_
+#define INCLUDE_PID_HPP_
 
 /**
- * @brief Defines the PID controller class. 
- * 
+ * @brief Defines the PID controller class.
+ *
  */
 class PID {
  public:
-    /**
-    * @brief Sets the Kp gain
-    * 
-    * @param kp Proportional Gain
-    * @return double Proportional Gain
-    */
-    double set_Kp(double kp);
+  /**
+   * @brief Sets the Kp gain
+   *
+   * @param kp Proportional Gain
+   * @return double Proportional Gain
+   */
+  double set_Kp(double kp);
 
-    /**
-     * @brief Sets the Ki gain
-     * 
-     * @param ki Integral Gain
-     * @return double Integral Gain
-     */
-    double set_Ki(double ki);
+  /**
+   * @brief Sets the Ki gain
+   *
+   * @param ki Integral Gain
+   * @return double Integral Gain
+   */
+  double set_Ki(double ki);
 
-    /**
-     * @brief Sets the Kd gain
-     * 
-     * @param kd Derivative Gain 
-     * @return double Derivative Gain
-     */
-    double set_Kd(double kd);
+  /**
+   * @brief Sets the Kd gain
+   *
+   * @param kd Derivative Gain
+   * @return double Derivative Gain
+   */
+  double set_Kd(double kd);
 
-    /**
-     * @brief Set the dt value.
-     * 
-     * @param dt small time change (sec)
-     * @return double small time change
-     */
-    double set_dt(double dt);
+  /**
+   * @brief Set the dt value.
+   *
+   * @param dt small time change (sec)
+   * @return double small time change
+   */
+  double set_dt(double dt);
 
-    /**
-     * @brief Computes PID control action on the error until the error converges to 
-     * a small threshold value of 0.01. Returns the new velocity of the mobile robot. 
-     *      
-     * @param ref_vel Reference Velocity (m/s)
-     * @param vel Current Velocity (m/s)
-     * @return double Control action (m/s) 
-     */
-    double compute(double ref_vel, double vel);
+  /**
+   * @brief Computes PID control action on the error until the error converges
+   * to a small threshold value of 0.01. Returns the new velocity of the mobile
+   * robot.
+   *
+   * @param ref_vel Reference Velocity (m/s)
+   * @param vel Current Velocity (m/s)
+   * @return double Control action (m/s)
+   */
+  double compute(double ref_vel, double vel);
 
  private:
-    double m_K_p;
-    double m_K_i;
-    double m_K_d;
-    double m_dt;    // small time change
+  double m_K_p;
+  double m_K_i;
+  double m_K_d;
+  double m_dt;  // small time change
 };
 
-#endif  // PID_HPP_
+#endif  // INCLUDE_PID_HPP_
